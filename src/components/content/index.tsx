6440b2ff54cef6d00d219ca4ffbaae1249723f73
uboznikov <uboznikov@outlook.com>
2023-03-05 21:29:51 +0800

    Step 4: Adding components
    Signed-off-by: uboznikov <uboznikov@outlook.com>

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,8 +1,31 @@
 import { h } from "preact";
+import "ojs/ojlabel";
+import "ojs/ojselectsingle";
+import { ojSelectSingle } from "ojs/ojselectsingle";
+import MutableArrayDataProvider = require("ojs/ojmutablearraydataprovider");
+
+type ChartType = {
+  id: number;
+  value: string;
+  label: string;
+};
+
+const chartTypeData = [
+  { value: "bar", label: "Bar" },
+  { value: "pie", label: "Pie" }
+];
+
+const chartTypesDP: MutableArrayDataProvider<ChartType["value"], ChartType>
+    = new MutableArrayDataProvider(chartTypeData, { keyAttributes: "value" });
 
 export function Content() {
   return (
     <div class="oj-web-applayout-max-width oj-web-applayout-content">
+      <h1>Product Information</h1>
+      <div id="itemDetailsContainer">
+        <oj-label for="basicSelect">Select Chart:</oj-label>
+        <oj-select-single id="basicSelect" value="pie" class="selectSingleStyle" data={chartTypesDP}></oj-select-single>
+      </div>
     </div>
   );
-};
+}
